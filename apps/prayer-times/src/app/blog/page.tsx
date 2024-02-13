59d3b8d0735b8000b46c62a021d43de2a01072a9
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-02-13 12:29:52 +0200

    feat: nextui init

---
+++ apps/prayer-times/src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "apps/prayer-times/src/components/primitives";
+
+export default function BlogPage() {
+	return (
+		<div>
+			<h1 className={title()}>Blog</h1>
+		</div>
+	);
+}
