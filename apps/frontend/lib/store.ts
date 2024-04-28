9c86f47510e64a25d4e5b6a5fdb8aa24648c6ea0
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-04-28 07:56:30 +0300

    fix: max call stack
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/frontend/lib/store.ts
+++ apps/frontend/lib/store.ts
@@ -1,6 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { TaskTimer } from 'tasktimer';
-import { publish } from '@enegix/events';
 import settingsSlice from './features/settings';
 import hadithSlice from './features/hadith';
 import timesSlice from './features/times';
@@ -18,31 +16,3 @@ const store = configureStore({
 });
 
 export default store;
-
-store.subscribe(() => {
-  const { times } = store.getState().times;
-
-  const newPrayer = times.find((time) => time.isNext);
-  if (!newPrayer) return;
-
-  const now = new Date();
-
-  const checkEvery = 1000;
-  const tasksQueue = new TaskTimer(checkEvery);
-
-  tasksQueue.add({
-    id: 'notify',
-    callback: () => {
-      if (newPrayer.time.getTime() - now.getTime() <= 0) {
-        console.log(`It's time for from store ${newPrayer.name}`);
-        publish('next-prayer', newPrayer);
-      } else {
-        // console.log(
-        //   `Next prayer is ${newPrayer.name} in ${newPrayer.time.getTime() - now.getTime()}ms`
-        // );
-      }
-    },
-    removeOnCompleted: true,
-  });
-  tasksQueue.start();
-});
