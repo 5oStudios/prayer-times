ddd7313843d7d2c600d3fd31376d371a54a8e5a7
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-05-31 23:40:07 +0300

    fix: build

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,12 +1,28 @@
-// @ts-check
-/** @type {import('next').NextConfig} */
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/next');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
-module.exports = nextConfig;
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
