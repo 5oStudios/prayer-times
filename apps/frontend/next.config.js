779df4966cd8eb2ab05f7a9950ca5b2b74f76fd2
faragzz <a.khaled46462@gmail.com>
2024-07-15 14:20:46 +0300

    feat: pwa init

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -3,8 +3,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+  dest: 'public',
+});
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
@@ -26,13 +26,6 @@ const nextConfig = {
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
-  withPWA({
-    pwa: {
-      dest: 'public', // Specify where the PWA assets should be outputted
-      // Optionally configure other PWA settings here
-    },
-  }),
 ];
 
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = composePlugins(...plugins)(withPWA(nextConfig));
